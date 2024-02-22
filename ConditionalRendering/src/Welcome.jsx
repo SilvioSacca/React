e8ff2253b3a05ce35aa776040539ef14b293cf9b
Silvio Saccà <sacca.silvio8@gmail.com>
2024-02-22 17:10:22 +0100

    ConditionalRendering2 update

--- ConditionalRendering/src/Welcome.jsx
+++ ConditionalRendering/src/Welcome.jsx
@@ -10,6 +10,7 @@ export function Welcome({ name, age }) {
             {age > 18 && <p>Your age is {age}</p>}
             {age > 18 && age < 65 && <p>Your age is {age}</p>}
             {age > 18 && age < 65 && name === "John" && <p>Your age is {age}</p>}
+            {age < 18 && <p>Your age is too young!</p>}
         </div>
 
     )
